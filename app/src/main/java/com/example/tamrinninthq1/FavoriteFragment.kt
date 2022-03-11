d32c6235fce42ea3c719e0094eeca5e7be3ce0ba
Samin <bmst555m@gmail.com>
2022-03-11 12:39:18 +0330

    make favorite fragment work

--- app/src/main/java/com/example/tamrinninthq1/FavoriteFragment.kt
+++ app/src/main/java/com/example/tamrinninthq1/FavoriteFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TableRow
+import androidx.core.view.isGone
+import androidx.core.view.isVisible
+import com.example.tamrinninthq1.databinding.FragmentFavoriteBinding
 
 class FavoriteFragment : Fragment() {
-
+    lateinit var binding: FragmentFavoriteBinding
+    var rowList = mutableListOf<TableRow>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -18,7 +23,29 @@ class FavoriteFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite, container, false)
+        binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        initView()
+        return binding.root
+    }
+
+    fun initView() {
+        rowList.add(binding.r1)
+        rowList.add(binding.r2)
+        rowList.add(binding.r3)
+        rowList.add(binding.r4)
+        rowList.add(binding.r5)
+        rowList.add(binding.r6)
+        rowList.add(binding.r7)
+        rowList.add(binding.r8)
+        rowList.add(binding.r9)
+        rowList.add(binding.r10)
+        rowList.add(binding.r11)
+        rowList.add(binding.r12)
+        for (i in 0..11) {
+            if (isLike[i] == true) {
+                rowList[i].isVisible = true
+            }
+        }
     }
 
 }
